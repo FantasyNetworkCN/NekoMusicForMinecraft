52034f68a4e608b15b805039e63292041bd682ba
NyaNyagulugulu <qianxun114514@gmail.com>
2026-09-22 17:53:29 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,9 @@
+# Neko歌姬计划安卓版
+![](https://count.getloli.com/get/@:NekoMusicMinecraft?theme=moebooru)
+
+> [!TIP]
+> 🐾 **pc端入口**：[点击这里查看 Neko歌姬计划 pc版仓库](https://github.com/MinecraftNekoServer/NekoMusicForPc)
+> 🐾 **后端**：[点击这里查看 Neko歌姬计划 后端仓库](https://github.com/FantasyNetworkCN/NekoMusic)
+
+# 本平台唯一官网 https://music.cnmsb.xin
+- ## 请勿通过非官方渠道获取安装包。本站被大量非法黑产生成盗版客户端诈骗，如你通过第三方平台获取安装包导致被骗本站拒绝一切赔偿
---
+++ src/main/java/xin/neko/fantasynetwork/Main.java
@@ -0,0 +1,14 @@
+package xin.neko.fantasynetwork;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "nekomusicforminecraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
