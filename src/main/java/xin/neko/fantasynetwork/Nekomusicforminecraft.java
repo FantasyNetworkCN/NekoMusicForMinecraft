24f66d999c9a266219693ca0b5116b1a51a4f424
NyaNyagulugulu <qianxun114514@gmail.com>
2026-09-22 17:29:28 +0800

    first commit

---
+++ src/main/java/xin/neko/fantasynetwork/Nekomusicforminecraft.java
@@ -0,0 +1,10 @@
+package xin.neko.fantasynetwork;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nekomusicforminecraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
